964380b2f31218c6df38b2dd43c98407b6e38b6a
Adelina-hub <adelina.macarascu@gmail.com>
2025-02-02 20:42:32 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDom.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.querySelector("#root")
 );
